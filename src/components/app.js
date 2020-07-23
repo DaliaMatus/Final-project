16ab41d484409cc233a456df67ef962073e7e705
DaliaMatus <daliamatus@gmail.com>
2020-07-22 23:42:00 -0500

    create the app.js base structure and styles

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,29 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Ecommerce</h1>
-        
+        <header className='app__header'>
+          <div className='app__header-first'>
+            <a href='app.js'>Sweatshirts</a>
+          </div>
+          <div className='app__header-second'>
+            <a href='cart.js'>Cart</a>
+            <a href='signin.js'>Sign in</a>
+          </div>
+        </header>
+        <main className='app__main'>
+          <div className='app__main-content'>
+          <ul className='app__main-products'>
+            <li>
+              <div className='app__main-product'>
+                 <img src='../../static/assets/sudadera-gris.jpg' alt='product'/>
+              </div>
+            </li>
+          </ul>
+          </div>
+        </main>
+        <footer className='app__footer'>
+          ©1997-2020  All right reserved
+        </footer>        
       </div>
     );
   }
